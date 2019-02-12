bdd045e2bdecb0185597098ec43f89c05f04a7dd
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-02-12 11:39:50 +0300

    shopping cart style

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="container">Корзина покупок {{ storeCount }}</div>
+    <div class="cart">
+        <div class="cart-counter">{{ storeCount }}</div>
+    </div>
 </template>
 
 <script>
@@ -15,9 +17,24 @@ export default {
 </script>
 
 <style scoped>
-.container {
-    padding: 20px;
+.cart {
+    width: 40px;
+    height: 40px;
     margin-left: auto;
-    margin-right: 35px;
+    margin-right: 50px;
+    position: relative;
+    background-image: url(../assets/icons/shopping-cart.svg);
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: contain;
+}
+.cart-counter {
+    background: #fff;
+    border-radius: 50%;
+    padding: 5px 10px;
+    position: absolute;
+    top: -10px;
+    right: -25px;
+    color: var(--color-dark)
 }
 </style>
