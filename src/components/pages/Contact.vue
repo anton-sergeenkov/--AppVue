cc67a746e761ec34bd574c9fb61f449b5c830c9e
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-03-04 16:03:12 +0300

    adde Carousel

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="wrapper">
         <h1>Контакты</h1>
+        <div class="map">
+            
+        </div>
     </div>
 </template>
--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,5 +1,22 @@
 <template>
     <div class="wrapper">
-        <h1>Главная</h1>
+        <v-carousel>
+            <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
+        </v-carousel>
     </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            items: [
+                {src: 'https://images.unsplash.com/photo-1480365334925-2aee561aa28e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=773&q=80'},
+                {src: 'https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'},
+                {src: 'https://images.unsplash.com/photo-1518890414976-bb41382be43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'},
+                {src: 'https://images.unsplash.com/photo-1500630417200-63156e226754?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'}       
+            ]
+        }
+    }
+}
+</script>
